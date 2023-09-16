bc6659ce124d01f49b16ba472354544648ab3b2f
SEBASTIAN BEDOYA GIL <sebastianbedoya151125@correo.itm.edu.co>
2023-09-16 11:55:03 -0500

    Create a new class for CommissionEmployee

---
+++ EmployeeOOP/Classes/CommissionEmployee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeOOP.Classes
+{
+    public class CommissionEmployee : Employee
+    {
+   
+
+        public float CommissionPercentage { get; set; }
+        public decimal Sales { get; set; }
+
+        
+
+   
+        public override decimal GetValueToPay()
+        {
+
+            return Sales 
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}\n\t" +
+                $"Commission Percentage.....{CommissionPercentage:P2}\n\t" +
+                $"Sales.....................{Sales:c2}\n\t" +
+                $"Value to Pay..............{GetValueToPay():C2}";
+        }
+
+       
+    }
+}
+
