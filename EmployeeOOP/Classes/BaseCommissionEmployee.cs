0125bac0f189f24972ae49badfac6a5cb19e8b01
SEBASTIAN BEDOYA GIL <sebastianbedoya151125@correo.itm.edu.co>
2023-09-16 12:10:09 -0500

    Completed with ToString() overload - BaseCommissionEmployee : CommissionEmployee

--- EmployeeOOP/Classes/BaseCommissionEmployee.cs
+++ EmployeeOOP/Classes/BaseCommissionEmployee.cs
@@ -4,7 +4,26 @@ using System.Text;
 
 namespace EmployeeOOP.Classes
 {
-    internal class BaseCommissionEmployee
+    public class BaseCommissionEmployee : CommissionEmployee
     {
+
+
+        public decimal Base { get; set; }
+
+
+        public override decimal GetValueToPay()
+        {
+            return base.GetValueToPay() + Base;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}\n\t" +
+                $"Base......................{Base:C2}\n\t" +
+                $"Total Value to pay........{GetValueToPay():C2}";
+        }
+
+
     }
 }
+
